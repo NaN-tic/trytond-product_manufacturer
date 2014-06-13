139c423c2e49f307af5502243b83c523b63f709d
resteve <resteve@zikzakmedia.com>
2014-06-13 13:38:16 +0200

    Setup

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
-#This file is part product_manufacturer module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
+#This file is part product_oneclick module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 
 from setuptools import setup
 import re
+import os
 import ConfigParser
+
+MODULE = 'product_manufacturer'
+PREFIX = 'trytonzz'
+MODULE2PREFIX = {}
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 config = ConfigParser.ConfigParser()
 config.readfp(open('tryton.cfg'))
@@ -20,28 +28,32 @@ minor_version = int(minor_version)
 requires = []
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res|webdav)(\W|$)', dep):
-        requires.append('trytond_%s >= %s.%s, < %s.%s' %
-                (dep, major_version, minor_version, major_version,
-                    minor_version + 1))
+        prefix = MODULE2PREFIX.get(dep, 'trytond')
+        requires.append('%s_%s >= %s.%s, < %s.%s' %
+                (prefix, dep, major_version, minor_version,
+                major_version, minor_version + 1))
 requires.append('trytond >= %s.%s, < %s.%s' %
         (major_version, minor_version, major_version, minor_version + 1))
 
-setup(name='trytonzz_product_manufacturer',
+tests_require = ['proteus >= %s.%s, < %s.%s' %
+    (major_version, minor_version, major_version, minor_version + 1)]
+
+setup(name='%s_%s' % (PREFIX, MODULE),
     version=info.get('version', '0.0.1'),
-    description='Tryton module add Manufacturer Product',
+    description='Tryton module allows manufacturer management in product',
     author='Zikzakmedia SL',
     author_email='zikzak@zikzakmedia.com',
     url='http://www.zikzakmedia.com',
-    download_url="https://bitbucket.org/zikzakmedia/trytond-product_manufacturer",
-    package_dir={'trytond.modules.product_manufacturer': '.'},
+    download_url="https://bitbucket.org/zikzakmedia/trytond-%s" % MODULE,
+    package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
-        'trytond.modules.product_manufacturer',
-        'trytond.modules.product_manufacturer.tests',
-    ],
+        'trytond.modules.%s' % MODULE,
+        'trytond.modules.%s.tests' % MODULE,
+        ],
     package_data={
-        'trytond.modules.product_manufacturer': info.get('xml', []) \
+        'trytond.modules.%s' % MODULE: (info.get('xml', [])
             + ['tryton.cfg', 'view/*.xml', 'locale/*.po']),
-    },
+        },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
@@ -57,14 +69,14 @@ setup(name='trytonzz_product_manufacturer',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Office/Business',
-    ],
+        ],
     license='GPL-3',
     install_requires=requires,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    product_manufacturer = trytond.modules.product_manufacturer
-    """,
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
 )
