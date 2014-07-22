e212aef2afee764b8953e3f5ed9335cf8afb0dc3
Guillem Barba <guillem@nan-tic.com>
2014-07-22 16:35:41 +0200

    Add party_manufacturer in dependencies and removed extension of party.party

--- __init__.py
+++ __init__.py
@@ -1,9 +1,8 @@
 #This file is part product_manufacturer module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
+#The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 from trytond.pool import Pool
 from .move import *
-from .party import *
 from .product import *
 from .purchase import *
 from .sale import *
@@ -12,7 +11,6 @@ from .sale import *
 def register():
     Pool.register(
         Move,
-        Party,
         Template,
         Product,
         PurchaseLine,
