3bd8eb30380bc82aba5d4d77ec2e0ab831697422
Raimon Esteve <raimon@nan-tic.com>
2017-06-08 11:53:01 +0200

    Remove on_chnage_product in depends

--- purchase.py
+++ purchase.py
@@ -1,5 +1,5 @@
 #This file is part product_manufacturer module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
+#The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import PoolMeta
@@ -23,9 +23,9 @@ class PurchaseLine:
                 else None)
         return result
 
-    @fields.depends('on_change_product')
     def on_change_product(self):
         super(PurchaseLine, self).on_change_product()
+
         self.manufacturer = None
         if self.product:
             self.manufacturer = self.product.manufacturer or None
